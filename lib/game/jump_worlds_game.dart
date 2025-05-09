26dc169eb54028104b5d096ab1e10020797f3e4a
Sang Doan <std35@georgetown.edu>
2025-05-09 16:58:03 -0700

    setup

---
+++ lib/game/jump_worlds_game.dart
@@ -0,0 +1,8 @@
+import 'package:flame/game.dart';
+
+class JumpWorldsGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    // Load your game assets and initialize components here
+  }
+}
